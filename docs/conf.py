1d5653f74d40002e9bdff542c50ec0aa9cd86e06
ccdonoso <75499243+ccdonoso@users.noreply.github.com>
2024-03-06 17:50:48 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['nbsphinx', 'sphinx_rtd_theme', "sphinx.ext.napoleon",
-    "pyvista.ext.plot_directive"]
+extensions = ['nbsphinx', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
